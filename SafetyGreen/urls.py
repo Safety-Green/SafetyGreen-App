5613dccf65424df62204c9005ec2bb73aa326754
Gu1Lima <gui03225@gmail.com>
2023-03-06 19:49:05 -0300

    consertado

--- SafetyGreen/urls.py
+++ SafetyGreen/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from SafetyGreen import views
-
 urlpatterns = [
-    path('', views.opp),
+    path('login/', include('login.urls')),
+    path('', include('login.urls')),
     path('admin/', admin.site.urls),
 ]
