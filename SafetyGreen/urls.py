a6e44a490ca5d267070f191903580823c10921c8
Gu1Lima <gui03225@gmail.com>
2023-03-06 15:19:21 -0300

    test: test blank url in urls

--- SafetyGreen/urls.py
+++ SafetyGreen/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('login/', include('login.urls')),
+    path('', include('login.urls')),
     path('admin/', admin.site.urls),
 ]
