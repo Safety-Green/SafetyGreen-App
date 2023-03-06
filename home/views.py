7eb2a6a232ba1307b7195b3d8b9cc58a02fca291
Gu1Lima <gui03225@gmail.com>
2023-03-06 15:38:35 -0300

    fix: created template to fix 404 HTML source

--- home/views.py
+++ home/views.py
@@ -1,10 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+
 
 def index(request):
-    return render(request, "homepage.html")
+    return render(request, "index.html")
     #return HttpResponse("informe seu cpf e sua senha para acessar sua dashboard")
 
 
